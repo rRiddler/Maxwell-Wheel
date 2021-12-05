36bd50f80b80eeceab4395a8431b730daf03ffbd
Вячеслав Михайлов <pmi02.mikhailov@gmail.com>
2021-12-06 00:33:16 +0700

    ITWORKS not really. Added all vars and being drawen and moved but not correctly.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -3,6 +3,37 @@ namespace Maxwell_Wheel
 {
     partial class MainForm
     {
+        public const float pi = 3.1415926f;
+        public const float g = 9.80665f;
+        static public float flag = 1;
+        static public float k = momentInert / (wheelRad * wheelRad) + wheelMass;
+        static public float a = -wheelMass * g / (k * 60);
+
+        static public float hingeLength = 0.1f;
+        static public float hingeRad = 0.005f;
+        static public float hingeDense = aluminumDense;
+        static public float hingeMass = hingeDense * pi * hingeRad * hingeRad * hingeLength;
+
+        static public bool toWork = false;
+
+        static public float plasticDense = 1800;
+        static public float steelDense = 7700;
+        static public float aluminumDense = 2800;
+        static public float height = stringLength - stringLengthMin;
+        static public float heightMax = stringLengthMax;
+        static public float stringLength = 0;
+        static public float stringLengthMax = 1; //most low point
+        static public float stringLengthMin = wheelRad + 0.01f; //top point
+        static public float wheelRad = 0.05f;
+        static public float wheelWidth = 0.02f;
+        static public float wheelDense = 7700f;
+        static public float wheelMass = pi * wheelRad * wheelRad * wheelDense * wheelWidth;
+        static public float momentInert = wheelMass * (wheelRad * wheelRad / 4 + wheelWidth * wheelWidth / 12) + hingeMass * (hingeRad * hingeRad / 4 + hingeLength * hingeLength / 12);
+        static public float energyFull = wheelMass * g * heightMax;
+        static public float energyPotential = wheelMass * g * height;
+        static public float energyKinetic = energyFull - energyPotential;
+        static public float velocity = 0;
+        static public float w = velocity * 180 / (wheelRad * pi);
         /// <summary>
         /// Обязательная переменная конструктора.
         /// </summary>
@@ -31,11 +62,21 @@ namespace Maxwell_Wheel
         {
             this.tabSheetSidePanel = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.numericUpDownWheelRad = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownWheelWidth = new System.Windows.Forms.NumericUpDown();
+            this.numericStringLength = new System.Windows.Forms.NumericUpDown();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.sidePanel = new System.Windows.Forms.Panel();
             this.sidePanelButton = new System.Windows.Forms.Button();
             this.mainScene = new SharpGL.SceneControl();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             this.tabSheetSidePanel.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWheelRad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWheelWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericStringLength)).BeginInit();
             this.sidePanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.mainScene)).BeginInit();
             this.SuspendLayout();
@@ -57,6 +98,11 @@ namespace Maxwell_Wheel
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.buttonStop);
+            this.tabPage1.Controls.Add(this.buttonStart);
+            this.tabPage1.Controls.Add(this.numericUpDownWheelRad);
+            this.tabPage1.Controls.Add(this.numericUpDownWheelWidth);
+            this.tabPage1.Controls.Add(this.numericStringLength);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
@@ -65,6 +111,114 @@ namespace Maxwell_Wheel
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(33, 360);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 5;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonStart.Location = new System.Drawing.Point(33, 234);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // numericUpDownWheelRad
+            // 
+            this.numericUpDownWheelRad.DecimalPlaces = 2;
+            this.numericUpDownWheelRad.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            196608});
+            this.numericUpDownWheelRad.Location = new System.Drawing.Point(12, 67);
+            this.numericUpDownWheelRad.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            131072});
+            this.numericUpDownWheelRad.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            196608});
+            this.numericUpDownWheelRad.Name = "numericUpDownWheelRad";
+            this.numericUpDownWheelRad.Size = new System.Drawing.Size(104, 20);
+            this.numericUpDownWheelRad.TabIndex = 2;
+            this.numericUpDownWheelRad.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            131072});
+            this.numericUpDownWheelRad.ValueChanged += new System.EventHandler(this.numericUpDownWheelRad_ValueChanged);
+            // 
+            // numericUpDownWheelWidth
+            // 
+            this.numericUpDownWheelWidth.DecimalPlaces = 2;
+            this.numericUpDownWheelWidth.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            196608});
+            this.numericUpDownWheelWidth.Location = new System.Drawing.Point(12, 125);
+            this.numericUpDownWheelWidth.Maximum = new decimal(new int[] {
+            0,
+            8,
+            0,
+            65536});
+            this.numericUpDownWheelWidth.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDownWheelWidth.Name = "numericUpDownWheelWidth";
+            this.numericUpDownWheelWidth.Size = new System.Drawing.Size(104, 20);
+            this.numericUpDownWheelWidth.TabIndex = 1;
+            this.numericUpDownWheelWidth.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDownWheelWidth.ValueChanged += new System.EventHandler(this.numericUpDownWheelWidth_ValueChanged);
+            // 
+            // numericStringLength
+            // 
+            this.numericStringLength.DecimalPlaces = 2;
+            this.numericStringLength.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericStringLength.Location = new System.Drawing.Point(12, 23);
+            this.numericStringLength.Maximum = new decimal(new int[] {
+            13,
+            0,
+            0,
+            65536});
+            this.numericStringLength.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            65536});
+            this.numericStringLength.Name = "numericStringLength";
+            this.numericStringLength.Size = new System.Drawing.Size(104, 20);
+            this.numericStringLength.TabIndex = 0;
+            this.numericStringLength.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            65536});
+            this.numericStringLength.ValueChanged += new System.EventHandler(this.numericStringLength_ValueChanged);
             // 
             // tabPage2
             // 
@@ -109,14 +263,16 @@ namespace Maxwell_Wheel
             | System.Windows.Forms.AnchorStyles.Right)));
             this.mainScene.Cursor = System.Windows.Forms.Cursors.Hand;
             this.mainScene.DrawFPS = true;
-            this.mainScene.FrameRate = 144;
-            this.mainScene.Location = new System.Drawing.Point(-14, -32);
+            this.mainScene.FrameRate = 120;
+            this.mainScene.Location = new System.Drawing.Point(0, -1);
             this.mainScene.Name = "mainScene";
             this.mainScene.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
             this.mainScene.RenderContextType = SharpGL.RenderContextType.FBO;
             this.mainScene.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
-            this.mainScene.Size = new System.Drawing.Size(1280, 720);
+            this.mainScene.Size = new System.Drawing.Size(1266, 680);
             this.mainScene.TabIndex = 2;
+            this.mainScene.OpenGLDraw += new SharpGL.RenderEventHandler(this.mainScene_Draw);
+            this.mainScene.Load += new System.EventHandler(this.mainScene_Load);
             // 
             // MainForm
             // 
@@ -130,6 +286,10 @@ namespace Maxwell_Wheel
             this.Name = "MainForm";
             this.Text = "Maxwell\'s Wheel Virtual Laboratory";
             this.tabSheetSidePanel.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWheelRad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWheelWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericStringLength)).EndInit();
             this.sidePanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.mainScene)).EndInit();
             this.ResumeLayout(false);
@@ -144,6 +304,12 @@ namespace Maxwell_Wheel
         private System.Windows.Forms.Panel sidePanel;
         private System.Windows.Forms.Button sidePanelButton;
         private SharpGL.SceneControl mainScene;
+        private System.Windows.Forms.NumericUpDown numericUpDownWheelWidth;
+        private System.Windows.Forms.NumericUpDown numericStringLength;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.NumericUpDown numericUpDownWheelRad;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -11,18 +11,39 @@ using System.Windows.Forms;
 using SharpGL.SceneGraph.Effects;
 using SharpGL.SceneGraph.Primitives;
 using SharpGL.Serialization.Wavefront;
-
+using System.Runtime;
 
 namespace Maxwell_Wheel
 {
-
     public partial class MainForm : Form
     {
-
         private readonly ArcBallEffect arcBallEffect = new ArcBallEffect();
+
+
+
 
         public MainForm()
         {
+            height = stringLength - stringLengthMin;
+            heightMax = stringLengthMax;
+            stringLength = 0;
+            stringLengthMax = 1; //most low point
+            stringLengthMin = wheelRad + 0.01f; //top point
+            wheelRad = 0.004f;
+            wheelWidth = 0.04f;
+            wheelDense = plasticDense;
+            wheelMass = pi * wheelRad * wheelRad * wheelDense * wheelWidth;
+            momentInert = wheelMass * (wheelRad * wheelRad / 4 + wheelWidth * wheelWidth / 12) + hingeMass * (hingeRad * hingeRad / 4 + hingeLength * hingeLength / 12);
+            energyFull = wheelMass * g * heightMax;
+            energyPotential = wheelMass * g * height;
+            energyKinetic = energyFull - energyPotential;
+            velocity = 0;
+            w = velocity * 180 / (wheelRad * pi);
+
+
+            k = momentInert / (wheelRad * wheelRad) + wheelMass;
+            a = -wheelMass * g / (k * 360);
+
             InitializeComponent();
             InitializeScene();
             this.CenterToScreen();
@@ -30,73 +51,40 @@ namespace Maxwell_Wheel
 
         private void InitializeScene()
         {
+            mainScene.Scene.RenderBoundingVolumes = false;
             mainScene.MouseDown += new MouseEventHandler(sceneControl_MouseDown);
             mainScene.MouseMove += new MouseEventHandler(sceneControl_MouseMove);
             mainScene.MouseUp += new MouseEventHandler(sceneControl_MouseUp);
-            // Let's load ducky
+
+            // Let's load project
             var obj = new ObjFileFormat();
-            var objScene = obj.LoadData(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "Full.obj"));
-            var objBase = obj.LoadData(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "Full.obj"));
+            var objWheel = obj.LoadData(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "Wheel.obj"));
+            var objBase = obj.LoadData(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "Only base.obj"));
+            var objStrings = obj.LoadData(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "Strings.obj"));
             // Add the materials to the scene
-            foreach (var asset in objScene.Assets)
+            foreach (var asset in objWheel.Assets)
                 mainScene.Scene.Assets.Add(asset);
-
-            // Get the polygons
-            var polygons = objScene.SceneContainer.Traverse<Polygon>().ToList();
-
-            // Add each polygon (There is only one in ducky.obj)
-            foreach (var polygon in polygons)
-            {
-                polygon.Name = "Qube";
-                polygon.Transformation.RotateX = 90f; // So that Ducky appears in the right orientation
-
-                //  Get the bounds of the polygon.
-                var boundingVolume = polygon.BoundingVolume;
-                var extent = new float[3];
-                polygon.BoundingVolume.GetBoundDimensions(out extent[0], out extent[1], out extent[2]);
-
-                // Get the max extent.
-                var maxExtent = extent.Max();
-
-                // Scale so that we are at most 10 units in size.
-                var scaleFactor = maxExtent > 10 ? 10.0f / maxExtent : 1;
-
-                polygon.Parent.RemoveChild(polygon);
-                polygon.Transformation.ScaleX = scaleFactor;
-                polygon.Transformation.ScaleY = scaleFactor;
-                polygon.Transformation.ScaleZ = scaleFactor;
-                polygon.Freeze(mainScene.OpenGL);
-                mainScene.Scene.SceneContainer.AddChild(polygon);
-
-                // Add effects.
-                polygon.AddEffect(new OpenGLAttributesEffect());
-                polygon.AddEffect(arcBallEffect);
-            }
-
             foreach (var asset in objBase.Assets)
                 mainScene.Scene.Assets.Add(asset);
+            foreach (var asset in objStrings.Assets)
+                mainScene.Scene.Assets.Add(asset);
 
             // Get the polygons
+            var polygonsWheel = objWheel.SceneContainer.Traverse<Polygon>().ToList();
             var polygonsBase = objBase.SceneContainer.Traverse<Polygon>().ToList();
+            var polygonsStrings = objStrings.SceneContainer.Traverse<Polygon>().ToList();
 
             // Add each polygon (There is only one in ducky.obj)
-            foreach (var polygon in polygonsBase)
+            foreach (var polygon in polygonsWheel)
             {
-                polygon.Name = "Qube";
+                polygon.Name = "Wheel";
                 polygon.Transformation.RotateX = 90f; // So that Ducky appears in the right orientation
 
-                //  Get the bounds of the polygon.
-                var boundingVolume = polygon.BoundingVolume;
-                var extent = new float[3];
-                polygon.BoundingVolume.GetBoundDimensions(out extent[0], out extent[1], out extent[2]);
-
-                // Get the max extent.
-                var maxExtent = extent.Max();
-
                 // Scale so that we are at most 10 units in size.
-                var scaleFactor = maxExtent > 10 ? 10.0f / maxExtent : 1;
+                var scaleFactor = 0.2f;
 
                 polygon.Parent.RemoveChild(polygon);
+                polygon.Transformation.TranslateZ = 8f;
                 polygon.Transformation.ScaleX = scaleFactor;
                 polygon.Transformation.ScaleY = scaleFactor;
                 polygon.Transformation.ScaleZ = scaleFactor;
@@ -107,6 +95,111 @@ namespace Maxwell_Wheel
                 polygon.AddEffect(new OpenGLAttributesEffect());
                 polygon.AddEffect(arcBallEffect);
             }
+
+            foreach (var polygon in polygonsBase)
+            {
+                polygon.Name = "Base";
+                polygon.Transformation.RotateX = 90; // So that Ducky appears in the right orientation
+
+                // Scale so that we are at most 10 units in size.
+                var scaleFactor = 0.2f;
+
+                polygon.Parent.RemoveChild(polygon);
+                polygon.Transformation.TranslateX = -0;
+                polygon.Transformation.TranslateY = -0;
+                polygon.Transformation.TranslateZ = 4f;
+                polygon.Transformation.ScaleX = scaleFactor;
+                polygon.Transformation.ScaleY = scaleFactor;
+                polygon.Transformation.ScaleZ = scaleFactor;
+                polygon.Freeze(mainScene.OpenGL);
+                mainScene.Scene.SceneContainer.AddChild(polygon);
+                float alf = polygon.Transformation.ScaleZ;
+                SharpGL.SceneGraph.Vertex bet = polygon.Transformation.TranslationVertex;
+                // Add effects.
+                polygon.AddEffect(new OpenGLAttributesEffect());
+                polygon.AddEffect(arcBallEffect);
+            }
+
+            foreach (var polygon in polygonsStrings)
+            {
+                polygon.Name = "Strings";
+                polygon.Transformation.RotateX = 90f; // So that Ducky appears in the right orientation
+
+                // Scale so that we are at most 10 units in size.
+                var scaleFactor = 0.2f;
+
+                polygon.Parent.RemoveChild(polygon);
+                polygon.Transformation.TranslateZ = 8f;
+                polygon.Transformation.ScaleX = scaleFactor;
+                polygon.Transformation.ScaleY = scaleFactor;
+                polygon.Transformation.ScaleZ = scaleFactor;
+                polygon.Freeze(mainScene.OpenGL);
+                mainScene.Scene.SceneContainer.AddChild(polygon);
+
+                // Add effects.
+                polygon.AddEffect(new OpenGLAttributesEffect());
+                polygon.AddEffect(arcBallEffect);
+            }
+
+        }
+
+        private void mainScene_Draw(object sender, SharpGL.RenderEventArgs args)
+        {
+            if (toWork)
+            {
+                var polygonsWheel = mainScene.Scene.SceneContainer.Traverse<Polygon>().Where<Polygon>(x => x.Name == "Wheel").ToList();
+                var polygonsStrings = mainScene.Scene.SceneContainer.Traverse<Polygon>().Where<Polygon>(x => x.Name == "Strings").ToList();
+                bool skip = false;
+                energyPotential = wheelMass * height * g;
+                energyKinetic = energyFull - energyPotential;
+                velocity += a;
+                stringLength -= velocity;
+                height = stringLengthMax - stringLength;
+                if (stringLength >= stringLengthMax)
+                {
+                    
+                    foreach (var polygon in polygonsWheel)
+                    {
+                        //polygon.Transformation.TranslateZ += ((velocity  + (stringLength - stringLengthMax)) * 6 + (stringLength - stringLengthMax)) * 0.95f;
+                        polygon.Transformation.TranslateZ += ((velocity + (stringLength - stringLengthMax)) + (stringLength - stringLengthMax))*6; //ITWORKS DONT TOUCH
+                    }
+                    foreach (var polygon in polygonsStrings)
+                    {
+                        polygon.Transformation.TranslateZ += ((velocity + (stringLength - stringLengthMax)) + (stringLength - stringLengthMax) * 0.95f)*6; //ITWORKS DONT TOUCH
+                    }
+                    flag *= -1;
+                    velocity *= -0.95f;
+                    //velocity += a*()
+                    skip = true;
+                    if (velocity < 0.002f && velocity > -0.002f)
+                    {
+                        velocity = 0;
+                        a = 0;
+                    }
+                    stringLength = 2 * stringLengthMax - stringLength;
+                }
+                w = 180f * flag * velocity / wheelRad / pi;
+
+                //wheel
+                foreach (var polygon in polygonsWheel)
+                {
+                    polygon.Transformation.RotateX += w / 60;
+                    if (!skip)
+                        polygon.Transformation.TranslateZ += velocity*6;
+                }
+
+                //strings
+                foreach (var polygon in polygonsStrings)
+                {
+                    if (!skip)
+                        polygon.Transformation.TranslateZ += velocity*6;
+                }
+            }
+        }
+
+        private void mainScene_Load(object sender, EventArgs e)
+        {
+
         }
 
         private void sceneControl_MouseDown(object sender, MouseEventArgs e)
@@ -144,7 +237,115 @@ namespace Maxwell_Wheel
             }
         }
 
+        private void numericStringLength_ValueChanged(object sender, EventArgs e)
+        {
+            stringLengthMax = (float)numericStringLength.Value;
 
+            height = stringLength - stringLengthMin;
+            heightMax = stringLengthMax;
+            stringLengthMin = wheelRad + 0.01f; //top point
+            wheelDense = plasticDense;
+            wheelMass = pi * wheelRad * wheelRad * wheelDense * wheelWidth;
+            momentInert = wheelMass * (wheelRad * wheelRad / 4 + wheelWidth * wheelWidth / 12) + hingeMass * (hingeRad * hingeRad / 4 + hingeLength * hingeLength / 12);
+            energyFull = wheelMass * g * heightMax;
+            energyPotential = wheelMass * g * height;
+            energyKinetic = energyFull - energyPotential;
+            velocity = 0;
+            w = velocity * 180 / (wheelRad * pi);
+
+
+            k = momentInert / (wheelRad * wheelRad) + wheelMass;
+            a = -wheelMass * g / (k * 360);
+        }
+
+        private void numericUpDownWheelWidth_ValueChanged(object sender, EventArgs e)
+        {
+            wheelWidth = (float)numericUpDownWheelWidth.Value;
+
+            height = stringLength - stringLengthMin;
+            heightMax = stringLengthMax;
+            stringLengthMin = wheelRad + 0.01f; //top point
+            wheelDense = plasticDense;
+            wheelMass = pi * wheelRad * wheelRad * wheelDense * wheelWidth;
+            momentInert = wheelMass * (wheelRad * wheelRad / 4 + wheelWidth * wheelWidth / 12) + hingeMass * (hingeRad * hingeRad / 4 + hingeLength * hingeLength / 12);
+            energyFull = wheelMass * g * heightMax;
+            energyPotential = wheelMass * g * height;
+            energyKinetic = energyFull - energyPotential;
+            velocity = 0;
+            w = velocity * 180 / (wheelRad * pi);
+
+
+            k = momentInert / (wheelRad * wheelRad) + wheelMass;
+            a = -wheelMass * g / (k * 360);
+        }
+
+        private void numericUpDownWheelRad_ValueChanged(object sender, EventArgs e)
+        {
+            wheelRad = (float)numericUpDownWheelRad.Value;
+
+            height = stringLength - stringLengthMin;
+            heightMax = stringLengthMax;
+            stringLengthMin = wheelRad + 0.01f; //top point
+            wheelDense = plasticDense;
+            wheelMass = pi * wheelRad * wheelRad * wheelDense * wheelWidth;
+            momentInert = wheelMass * (wheelRad * wheelRad / 4 + wheelWidth * wheelWidth / 12) + hingeMass * (hingeRad * hingeRad / 4 + hingeLength * hingeLength / 12);
+            energyFull = wheelMass * g * heightMax;
+            energyPotential = wheelMass * g * height;
+            energyKinetic = energyFull - energyPotential;
+            velocity = 0;
+            w = velocity * 180 / (wheelRad * pi);
+
+
+            k = momentInert / (wheelRad * wheelRad) + wheelMass;
+            a = -wheelMass * g / (k * 360);
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (toWork) { buttonStart.Text = "Start"; toWork = false; }
+            else { buttonStart.Text = "Pause"; toWork = true; }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            toWork = false;
+
+            stringLengthMax = (float)numericStringLength.Value;
+            wheelWidth = (float)numericUpDownWheelWidth.Value;
+            wheelRad = (float)numericUpDownWheelRad.Value;
+
+            height = stringLength - stringLengthMin;
+            heightMax = stringLengthMax;
+            stringLength = 0;
+            stringLengthMin = wheelRad + 0.01f; //top point
+            wheelDense = plasticDense;
+            wheelMass = pi * wheelRad * wheelRad * wheelDense * wheelWidth;
+            momentInert = wheelMass * (wheelRad * wheelRad / 4 + wheelWidth * wheelWidth / 12) + hingeMass * (hingeRad * hingeRad / 4 + hingeLength * hingeLength / 12);
+            energyFull = wheelMass * g * heightMax;
+            energyPotential = wheelMass * g * height;
+            energyKinetic = energyFull - energyPotential;
+            velocity = 0;
+            w = velocity * 180 / (wheelRad * pi);
+
+
+            k = momentInert / (wheelRad * wheelRad) + wheelMass;
+            a = -wheelMass * g / (k * 360);
+
+            var polygonsWheel = mainScene.Scene.SceneContainer.Traverse<Polygon>().Where<Polygon>(x => x.Name == "Wheel").ToList();
+            var polygonsStrings = mainScene.Scene.SceneContainer.Traverse<Polygon>().Where<Polygon>(x => x.Name == "Strings").ToList();
+
+            foreach (var polygon in polygonsWheel)
+            {
+                polygon.Transformation.TranslateZ = 8f;
+            }
+
+            //strings
+            foreach (var polygon in polygonsStrings)
+            {
+                polygon.Transformation.TranslateZ = 8f;
+            }
+
+        }
     }
 
 
